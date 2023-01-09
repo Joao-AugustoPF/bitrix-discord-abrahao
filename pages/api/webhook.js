38261502b459a97bb3dbd5c430a10c467f3ffe93
João Augusto <joaoaugustopfpf@gmail.com>
2023-01-09 15:07:02 -0300

    teste 5

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -7,26 +7,6 @@ const bitrix = Bitrix(
 
 export default async function handler(req, res) {
     //const data = await axios.get("https://oimenu.bitrix24.com.br/rest/24088/y6nxi5u1y6p3l6ta/events.json")
-    await bitrix.deals.get(req.body["data[FIELDS][ID]"]).then(({ result }) => {
-        if (result.STAGE_ID === "EXECUTING") {
-            console.log("passei aqui");
-             axios.post(
-                "https://discordapp.com/api/webhooks/1060977635393544312/GNMePjBnzXEX96PbyT1QRPwwcdjnQTvjzr5Tdr-KHolZWsgqC6DJPOFt8olwyjzKZ39n",
-                {
-                    content: `⚠️⚠️⚠️ATENÇÃO @Suporte ⚠️⚠️⚠️ PRÉ GOLIVE - ${
-                        result.TITLE
-                    } - Info: ${result.UF_CRM_1673270284034} - Tablets: ${
-                        result.UF_CRM_1673272410518
-                            ? `${result.UF_CRM_1673272410518}`
-                            : "Não"
-                    } - Possui Pocket?: ${
-                        result.UF_CRM_1673272763373 === "1" ? "Sim" : "Não"
-                    }`,
-                }
-            );
-            console.log("executei");
-        }
-    });
     switch (req.body.event) {
         case "ONCRMDEALUPDATE":
            await bitrix.deals
